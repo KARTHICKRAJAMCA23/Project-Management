d10dc641826958cc939dfb17083b793158cab3b9
Karthick Raja <apple@MacBook-Air.local>
2025-10-03 14:57:00 +0530

    Initial commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://project-management-1-2rgk.onrender.com/api",
+  baseURL: ["http://localhost:5173/api","https://project-management-1-2rgk.onrender.com/api"],
 });
 
 API.interceptors.request.use((config) => {
